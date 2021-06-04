614e456efce9c37db93340cb1492eecccef79d03
Evan Raiford <84350467+ehraiford@users.noreply.github.com>
2021-06-04 19:37:24 -0400

    Completed Code of exercise 06.

--- src/main/java/ex06/App.java
+++ src/main/java/ex06/App.java
@@ -1,4 +1,24 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
 package ex06;
+import java.time.Year;
+import java.util.Scanner;
 
 public class App {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("What is your current age? ");
+        String cAge = in.nextLine();
+        int intCAge = Integer.parseInt(cAge);
+        System.out.print("At what age would you like to retire? ");
+        String rAge = in.nextLine();
+        int intRAge = Integer.parseInt(rAge);
+        int diff = intRAge - intCAge;
+        System.out.println("You have " + diff + " years left until you can retire.");
+        int year = Year.now().getValue(), yearRetire = year + diff;
+        System.out.print("It's " + year + ", so you can retire in " + yearRetire + ".");
+
+    }
 }
